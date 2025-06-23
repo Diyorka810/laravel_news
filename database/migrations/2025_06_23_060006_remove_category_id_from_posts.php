02608664dd284ea747d7739e8ed5ed9787b24394
Diyorka810 <nagibatordier@gmail.com>
2025-06-23 11:20:08 +0500

    Remove extra comment

--- database/migrations/2025_06_23_060006_remove_category_id_from_posts.php
+++ database/migrations/2025_06_23_060006_remove_category_id_from_posts.php
@@ -18,7 +18,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        // возвращаем всё как было
         DB::statement('ALTER TABLE posts ADD COLUMN category_id BIGINT');
         DB::statement('ALTER TABLE posts ADD CONSTRAINT posts_category_id_foreign
                        FOREIGN KEY (category_id) REFERENCES categories(id)
